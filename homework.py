e0f63eb77504830c4b72ffd1af401720ffde1989
Alex Kozhan <kozhanalex@gmail.com>
2024-05-10 23:05:09 +0300

    changed dockstrings in homework.py file

--- homework.py
+++ homework.py
@@ -1,19 +1,19 @@
 """Homework6"""
 
 
-# Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
+"""Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]"""
 FIRST_LINE = "Robin Singh"
 print(list(FIRST_LINE.split()))
 
 
-# "I love arrays they are my favorite" =>
-# ["I", "love", "arrays", "they", "are", "my", "favorite"]
+""" "I love arrays they are my favorite" =>
+["I", "love", "arrays", "they", "are", "my", "favorite"]"""
 SECOND_LINE = "I love arrays they are my favorite"
 print(list(SECOND_LINE.split()))
 
 
-# Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
-# Напечатайте текст: “Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus”
+"""Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
+Напечатайте текст: 'Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus'"""
 THIRD_LINE = ["Ivan", "Ivanov"]
 CITY_LINE = "Minsk"
 STRANA_LINE = "Belarus"
@@ -21,14 +21,14 @@ print(f"Привет, {' '.join(THIRD_LINE)}! "
       f"Добро пожаловать в {CITY_LINE} {STRANA_LINE}")
 
 
-# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
-# сделайте из него строку => "I love arrays they are my favorite"
+"""Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
+сделайте из него строку => '"'I love arrays they are my favorite'""""
 NEW_SPISOK = ["I", "love", "arrays", "they", "are", "my", "favorite"]
 print(" ".join(NEW_SPISOK))
 
 
-# Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение,
-# удалите элемент из списка под индексом 6
+"""Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение,
+удалите элемент из списка под индексом 6"""
 OLD_SPISOK = [
     "I", "love", "arrays", "they", "are",
     "my", "favorite", "python", "learning", "instrument"
