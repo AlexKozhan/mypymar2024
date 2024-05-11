28df0d0a6b96e23091276c36967237a76983d32a
Alex Kozhan <kozhanalex@gmail.com>
2024-05-11 15:42:55 +0300

    changed dockstrings in homework.py file

--- homework.py
+++ homework.py
@@ -1,43 +1,43 @@
 """Homework6"""
 
 
-""" This is a docstring which describes the module:
-Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]"""
+# Перевести строку в список "Robin Singh" => ["Robin”, “Singh"]
 FIRST_LINE = "Robin Singh"
+"""printing the list out of string"""
 print(list(FIRST_LINE.split()))
 
 
-""" This is a docstring which describes the module:
-"I love arrays they are my favorite" =>
-["I", "love", "arrays", "they", "are", "my", "favorite"]"""
+# "I love arrays they are my favorite" =>
+# ["I", "love", "arrays", "they", "are", "my", "favorite"]
 SECOND_LINE = "I love arrays they are my favorite"
+"""printing the list out of string"""
 print(list(SECOND_LINE.split()))
 
 
-"""This is a docstring which describes the module:
-Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
-Напечатайте текст: 'Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus'"""
+# Дан список: [Ivan, Ivanou], и 2 строки: Minsk, Belarus
+# Напечатайте текст: 'Привет, Ivan Ivanou! Добро пожаловать в Minsk Belarus'
 THIRD_LINE = ["Ivan", "Ivanov"]
 CITY_LINE = "Minsk"
 STRANA_LINE = "Belarus"
+"""printing the text out of string and list"""
 print(f"Привет, {' '.join(THIRD_LINE)}! "
       f"Добро пожаловать в {CITY_LINE} {STRANA_LINE}")
 
 
-"""This is a docstring which describes the module:
-Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
-сделайте из него строку => '"'I love arrays they are my favorite'"""
+# Дан список ["I", "love", "arrays", "they", "are", "my", "favorite"]
+# сделайте из него строку => '"'I love arrays they are my favorite'
 NEW_SPISOK = ["I", "love", "arrays", "they", "are", "my", "favorite"]
+"""printing the string out of list"""
 print(" ".join(NEW_SPISOK))
 
 
-"""This is a docstring which describes the module:
-Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение,
-удалите элемент из списка под индексом 6"""
+# Создайте список из 10 элементов, вставьте на 3-ю позицию новое значение,
+# удалите элемент из списка под индексом 6
 OLD_SPISOK = [
     "I", "love", "arrays", "they", "are",
     "my", "favorite", "python", "learning", "instrument"
     ]
 OLD_SPISOK[2] = "tuples"
 del OLD_SPISOK[6]
+"""printing the changed final list"""
 print(OLD_SPISOK)
