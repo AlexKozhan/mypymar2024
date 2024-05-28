7896cb297eb5878a33d198b09cb784a76c9ed5bc
Alex Kozhan <kozhanalex@gmail.com>
2024-05-29 00:11:05 +0300

    homework9 is done in 3 Python files

---
+++ homework1.py
@@ -0,0 +1,31 @@
+"""Напишите программу, которая бы работала
+следующим образом - находила символ "#" и если
+этот символ найден - удаляла предыдущий символ
+из строки. Ваша задача обработать строки с "#" символом.
+
+Примеры:
+
+"a#bc#d" ==>  "bd"
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+"""
+
+
+def sortirovka(a):
+    masiv = []
+    for i in a:
+       if i == '#':
+           if masiv:
+               masiv.pop()
+       else:
+           masiv.append(i)
+    return ''.join(masiv)
+
+
+print(sortirovka("a#bc#d"))
+print(sortirovka("abc#d##c"))
+print(sortirovka("abc##d######"))
+print(sortirovka("#######"))
+print(sortirovka(""))
---
+++ homework3.py
@@ -0,0 +1,44 @@
+"""
+На вход подается строка, например, "cccbba" результат
+работы программы - строка “c3b2a"
+
+Примеры для проверки работоспособности:
+
+"cccbba" == "c3b2a"
+"abeehhhhhccced" == "abe2h5c3ed"
+"aaabbceedd" == "a3b2ce2d2"
+"abcde" == "abcde"
+"aaabbdefffff" == "a3b2def5"
+"""
+
+
+def count_letters(s):
+    if not s:
+        return ""
+
+    result = []
+    current_char = s[0]
+    count = 1
+
+    for char in s[1:]:
+        if char == current_char:
+            count += 1
+        else:
+            result.append(current_char)
+            if count > 1:
+                result.append(str(count))
+            current_char = char
+            count = 1
+
+    result.append(current_char)
+    if count > 1:
+        result.append(str(count))
+
+    return ''.join(result)
+
+
+print(count_letters("cccbba"))
+print(count_letters("abeehhhhhccced"))
+print(count_letters("aaabbceedd"))
+print(count_letters("abcde"))
+print(count_letters("aaabbdefffff"))
