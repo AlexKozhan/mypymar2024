b38e7b1f3f41c86ac7f86e2489a20b44584f79f8
Alex Kozhan <kozhanalex@gmail.com>
2024-07-13 14:54:00 +0300

    linters

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,14 +1,23 @@
 """
-This module contains tests for the library system including the Book and User classes.
-It uses pytest for testing and logging to capture log messages.
+This module contains tests for the library system
+including the Book and User classes.
+It uses pytest for testing and logging to capture
+log messages.
 """
 
+# pylint: disable=E0401,C0411,W0621
+
+# standard library imports
+import logging
+
+# third party imports
 import pytest
 from library import Book, User
-import logging
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 @pytest.fixture
 def setup_library():
@@ -18,49 +27,64 @@ def setup_library():
     Returns:
         tuple: Contains two book objects and two user objects.
     """
-    book1 = Book("White Fang", "Jack London", 100, "1234567890")
-    book2 = Book("Animals", "Ivan Ivanov", 101, "2232442")
+    book1 = Book("White Fang", "Jack "
+                               "London", 100, "1234567890")
+    book2 = Book("Animals", "Ivan "
+                            "Ivanov", 101, "2232442")
     user1 = User("Alex")
     user2 = User("Siarhei")
     return book1, book2, user1, user2
+
 
 def test_reserve_book(setup_library, caplog):
     """
     Test reserving a book.
 
     Args:
-        setup_library (fixture): Fixture to set up the library.
-        caplog (LogCaptureFixture): Fixture to capture log messages.
+        setup_library (fixture): Fixture to
+        set up the library.
+        caplog (LogCaptureFixture): Fixture
+        to capture log messages.
     """
     book1, _, user1, _ = setup_library
     caplog.clear()
     with caplog.at_level(logging.INFO):
         assert user1.reserve_book(book1) is True
-    assert "Attempting to reserve book 'White Fang' by user Alex." in caplog.text
-    assert "Book 'White Fang' is reserved by user Alex." in caplog.text
+    assert ("Attempting to reserve book 'White Fang' "
+            "by user Alex.") in caplog.text
+    assert ("Book 'White Fang' is reserved "
+            "by user Alex.") in caplog.text
+
 
 def test_take_book(setup_library, caplog):
     """
     Test taking a book.
 
     Args:
-        setup_library (fixture): Fixture to set up the library.
-        caplog (LogCaptureFixture): Fixture to capture log messages.
+        setup_library (fixture): Fixture to
+        set up the library.
+        caplog (LogCaptureFixture): Fixture
+        to capture log messages.
     """
     book1, _, user1, _ = setup_library
     caplog.clear()
     with caplog.at_level(logging.INFO):
         assert user1.take_book(book1) is True
-    assert "Attempting to take book 'White Fang' by user Alex." in caplog.text
-    assert "Book 'White Fang' is taken by user Alex." in caplog.text
+    assert ("Attempting to take book 'White Fang' "
+            "by user Alex.") in caplog.text
+    assert ("Book 'White Fang' is taken "
+            "by user Alex.") in caplog.text
+
 
 def test_return_book(setup_library, caplog):
     """
     Test returning a book.
 
     Args:
-        setup_library (fixture): Fixture to set up the library.
-        caplog (LogCaptureFixture): Fixture to capture log messages.
+        setup_library (fixture): Fixture to
+        set up the library.
+        caplog (LogCaptureFixture): Fixture
+        to capture log messages.
     """
     book1, _, user1, _ = setup_library
     user1.reserve_book(book1)
@@ -68,5 +92,7 @@ def test_return_book(setup_library, caplog):
     caplog.clear()
     with caplog.at_level(logging.INFO):
         assert User.return_book(book1, user1) is True
-    assert "Attempting to return book 'White Fang' by user Alex." in caplog.text
-    assert "Book 'White Fang' is returned by user Alex." in caplog.text
+    assert ("Attempting to return book 'White Fang' "
+            "by user Alex.") in caplog.text
+    assert ("Book 'White Fang' is returned "
+            "by user Alex.") in caplog.text
