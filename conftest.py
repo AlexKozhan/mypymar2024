802b90055a72e33d68bdfddbe736de3d97cdcb57
Alex Kozhan <kozhanalex@gmail.com>
2024-08-08 18:39:28 +0300

    Homework is redone

--- conftest.py
+++ conftest.py
@@ -1,31 +1,14 @@
+"""Conftest module"""
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import test_data
 
 
 @pytest.fixture
 def driver():
-    """
-    Pytest fixture to set up the Selenium WebDriver with Chrome options.
-
-    This fixture initializes a Chrome WebDriver
-    instance with incognito mode and disabled
-    password manager options. The WebDriver instance
-    is provided to each test case, and
-    it automatically quits after the test execution to clean up resources.
-
-    Returns:
-        WebDriver: A Chrome WebDriver instance configured for the test session.
-    """
-    options = Options()
-    options.add_argument("--incognito")
-    options.add_experimental_option("prefs", {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False
-    })
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                              options=options)
-    yield driver
-    driver.quit()
+    """Fixture for initializing and quitting a Chrome WebDriver instance"""
+    chrome = webdriver.Chrome()
+    chrome.implicitly_wait(5)
+    chrome.get(test_data.url)
+    yield chrome
+    chrome.quit()
